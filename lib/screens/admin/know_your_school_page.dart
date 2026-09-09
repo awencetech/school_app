c2ffd0a17e7b5527a902f383aa4e7ad48d17a261
awencetech <awencetech@gmail.com>
2026-09-09 15:19:19 +0530

    Update Know Your School page navigation

--- lib/screens/admin/know_your_school_page.dart
+++ lib/screens/admin/know_your_school_page.dart
@@ -11,47 +11,103 @@ class KnowYourSchoolPage extends StatelessWidget {
   const KnowYourSchoolPage({super.key});
 
   final List<_SchoolTile> _tiles = const [
-    _SchoolTile(icon: Icons.language, label: 'Website', color: Color(0xFF4CAF50), route: AppRoutes.adminKnowYourSchoolWebsiteEdit),
-    _SchoolTile(icon: Icons.school, label: 'School handbook', color: Color(0xFFF59E0B), route: AppRoutes.adminKnowYourSchoolSchoolHandbookEdit),
-    _SchoolTile(icon: Icons.event, label: 'Events Celebrations', color: Color(0xFFF44336), route: AppRoutes.adminKnowYourSchoolEventsCelebrationEdit),
-    _SchoolTile(icon: Icons.folder_copy_outlined, label: 'School Res.', color: Color(0xFF8D6E63), route: AppRoutes.adminKnowYourSchoolSchoolResourcesEdit),
-    _SchoolTile(icon: Icons.newspaper, label: 'Newsletter', color: Color(0xFF5C84C3), route: AppRoutes.adminKnowYourSchoolNewsletterEdit),
-    _SchoolTile(icon: Icons.announcement, label: 'Announcement', color: Color(0xFF43A047), route: AppRoutes.adminKnowYourSchoolAnnouncementEdit),
-    _SchoolTile(icon: Icons.people, label: 'Demography', color: Color(0xFF6A1B9A), route: AppRoutes.adminKnowYourSchoolDemographyEdit),
-    _SchoolTile(icon: Icons.facebook, label: 'Facebook', color: Color(0xFF3B5998), route: AppRoutes.adminKnowYourSchoolFacebookEdit),
-    _SchoolTile(icon: Icons.ondemand_video, label: 'Youtube', color: Color(0xFFD32F2F), route: AppRoutes.adminKnowYourSchoolYoutubeEdit),
-    _SchoolTile(icon: Icons.chat, label: 'Whatsapp', color: Color(0xFF25D366), route: AppRoutes.adminKnowYourSchoolWhatsappEdit),
-    _SchoolTile(icon: Icons.camera_alt, label: 'Instagram', color: Color(0xFFE1306C), route: AppRoutes.adminKnowYourSchoolInstagramEdit),
-    _SchoolTile(icon: Icons.library_books, label: 'Library', color: Color(0xFF795548), route: AppRoutes.adminKnowYourSchoolLibraryEdit),
+    _SchoolTile(
+      icon: Icons.language,
+      label: 'Website',
+      color: Color(0xFF4CAF50),
+      route: AppRoutes.adminKnowYourSchoolWebsiteEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.school,
+      label: 'School handbook',
+      color: Color(0xFFF59E0B),
+      route: AppRoutes.adminKnowYourSchoolSchoolHandbookEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.event,
+      label: 'Events Celebrations',
+      color: Color(0xFFF44336),
+      route: AppRoutes.adminKnowYourSchoolEventsCelebrationEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.folder_copy_outlined,
+      label: 'School Res.',
+      color: Color(0xFF8D6E63),
+      route: AppRoutes.adminKnowYourSchoolSchoolResourcesEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.newspaper,
+      label: 'Newsletter',
+      color: Color(0xFF5C84C3),
+      route: AppRoutes.adminKnowYourSchoolNewsletterEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.announcement,
+      label: 'Announcement',
+      color: Color(0xFF43A047),
+      route: AppRoutes.adminKnowYourSchoolAnnouncementEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.people,
+      label: 'Demography',
+      color: Color(0xFF6A1B9A),
+      route: AppRoutes.adminKnowYourSchoolDemographyEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.facebook,
+      label: 'Facebook',
+      color: Color(0xFF3B5998),
+      route: AppRoutes.adminKnowYourSchoolFacebookEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.ondemand_video,
+      label: 'Youtube',
+      color: Color(0xFFD32F2F),
+      route: AppRoutes.adminKnowYourSchoolYoutubeEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.chat,
+      label: 'Whatsapp',
+      color: Color(0xFF25D366),
+      route: AppRoutes.adminKnowYourSchoolWhatsappEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.camera_alt,
+      label: 'Instagram',
+      color: Color(0xFFE1306C),
+      route: AppRoutes.adminKnowYourSchoolInstagramEdit,
+    ),
+    _SchoolTile(
+      icon: Icons.library_books,
+      label: 'Library',
+      color: Color(0xFF795548),
+      route: AppRoutes.adminKnowYourSchoolLibraryEdit,
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.background,
+      backgroundColor: AppColors.white,
       appBar: AppBar(
         backgroundColor: AppColors.topBar,
-        leading: IconButton(
-          onPressed: () => navigateBack(context),
-          icon: const Icon(Icons.arrow_back, color: AppColors.white),
-        ),
         title: const Text('Know Your School'),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 'Know your School',
                 style: GoogleFonts.poppins(
-                  fontSize: 18,
+                  fontSize: 16,
                   fontWeight: FontWeight.w600,
-                  color: AppColors.primaryText,
+                  color: const Color(0xFF222222),
                 ),
               ),
-              const SizedBox(height: 16),
+              const SizedBox(height: 10),
               DashboardIconGrid(
                 children: _tiles
                     .map(
@@ -59,7 +115,8 @@ class KnowYourSchoolPage extends StatelessWidget {
                         icon: tile.icon,
                         label: tile.label,
                         color: tile.color,
-                        onTap: () => Navigator.of(context).pushNamed(tile.route),
+                        onTap: () =>
+                            Navigator.of(context).pushNamed(tile.route),
                       ),
                     )
                     .toList(),
